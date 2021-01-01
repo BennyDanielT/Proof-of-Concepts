e6eacfbf1e7388f323bdab699daa9e7cbc89ee8b
Daniel <48117094+Tech-Knight-Danny@users.noreply.github.com>
2021-01-01 10:36:36 +0530

    Add files via upload

---
+++ Automation/Voice_Assistant_Beginner/Voice_Assistant_Beginner_Requirements.py
@@ -0,0 +1,12 @@
+
+---Mandatory Packages---
+
+Speech recognition Module: pip install speechRecognition
+
+Text to speech: pip install pyttsx3
+
+Advance control on browser: pip install pywhatkit
+
+Wikipedia module: pip install wikipedia
+
+Jokes: pip install pyjokes
